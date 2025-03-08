df348e46f85f245709d112cf6878145d05055727
greenehat <greene.aaronjames@gmail.com>
2025-03-08 10:27:09 -0700

    pygame venv and main file created

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("Hello World")
+# this allows us to use code from
+# the open-source pygame library
+# throughout this file
+
+import pygame
+
+print("Starting Asteroids!")
+
+
+
+if __name__ == "__main__":
+    main()
