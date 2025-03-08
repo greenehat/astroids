bbb7a178044f1c2b66b0c2bdc871bb2ead0a9acd
greenehat <greene.aaronjames@gmail.com>
2025-03-08 13:23:30 -0700

    game screen and quit button created

--- main.py
+++ main.py
@@ -7,12 +7,24 @@ from constants import *
 
 
 
-
-
 print("Starting Asteroids!")
 print(f"Screen width: {SCREEN_WIDTH}")
 print(f"Screen height: {SCREEN_HEIGHT}")
 
 
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        pygame.Surface.fill(screen,(0,0,0))
+        pygame.display.flip()
+
+
+
 if __name__ == "__main__":
+    
     main()
