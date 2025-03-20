83d89928706bd299bd295cf11497af62f0e7a789
greenehat <greene.aaronjames@gmail.com>
2025-03-19 22:26:17 -0600

    Finished player movement, added asteroid and asteroidfield classes

--- main.py
+++ main.py
@@ -5,7 +5,7 @@
 import pygame
 from constants import *
 from player import *
-
+from AsteroidField import *
 
 print("Starting Asteroids!")
 print(f"Screen width: {SCREEN_WIDTH}")
@@ -17,15 +17,26 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    astroids = pygame.sprite.Group()
+
+    Player.containers = (updateable, drawable)
+    Asteroid.containers = (astroids, updateable, drawable)
+    AsteroidField.containers = (updateable)
+
     player = Player((SCREEN_WIDTH / 2),(SCREEN_HEIGHT / 2))
+    asteroid_field = AsteroidField()
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         pygame.Surface.fill(screen,(0,0,0))
-        player.update(dt)
-        player.draw(screen)
+        updateable.update(dt)
+        for drawable_object in drawable:
+            drawable_object.draw(screen)
         pygame.display.flip()
         clock.tick(60)
         dt = clock.tick(60) / 1000
